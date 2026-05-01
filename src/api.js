fb6b64c7374967672362ca1bb02c7899e520bf13
Prashanth BKM <117796753+Prashanthbkm@users.noreply.github.com>
2026-05-01 20:35:47 +0530

    updated API base URL for production

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://travelmate-n9g5.onrender.com';
 
 // Axios instance to handle requests
 const apiClient = axios.create({
